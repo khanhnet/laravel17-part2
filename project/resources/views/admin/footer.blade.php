0c3d6c68feaa6fc82efba5e3353d347b6691b06a
unknown <khanhnet2632000@gmail.com>
2020-08-24 08:42:45 +0700

    del project

--- project/resources/views/admin/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer" style="background-color: #f7f7f7; height: 100px; padding-top: 30px">
-
-    <!-- Copyright -->
-    <div class="text-center">© 2019 Copyright:
-        <a href="#"> Nguyen Cong Hoan</a>
-    </div>
-    <!-- Copyright -->
-
-</footer>
--- project/resources/views/greeting.blade.php
+++
@@ -1,5 +0,0 @@
-<html>
-    <body>
-        <h1>Hello .{{$name}} age {{ $age }}</h1>
-    </body>
-</html>
